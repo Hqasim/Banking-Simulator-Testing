a4c6da653c8971a73c5fe71534fd488c001380d3
jordanadania <jordan.adania@gmail.com>
2020-04-20 09:38:59 -0400

    new skeleton attempt

---
+++ src/test/java/banking/Tester.java
@@ -0,0 +1,22 @@
+package banking;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+  * Java II - Professor Wayne Pollock
+  * Project 6 - Banking Simulator Testing
+  * Team - Thi, Hamzah, Jordan, Diane [THJD]
+  * HCC - Tampa, FL
+ */
+
+public class Tester{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue(){
+        assertTrue( true );
+    }
+}
